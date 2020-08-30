b354686956fdcf05370b03381f275d477be98b12
Kamil Bronisz <kam.bronisz@gmail.com>
2020-08-30 10:26:26 +0200

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
+import React, { Component } from `react`;
+import axios from `axios`;
 
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios'
-
-class App extends Component {
+class app extends Component {
   state = {
     response: {}
   };
-  
-  componentDidMount() {
-    axios.get('/say-something').then((res) => {
-      const response = res.data;
+
+  componentDidMouth () {
+    axios.get(`/say-something`).then((res) => {
+      const response= res.data;
       this.setState({response});
     });
   }
 
+
   render() {
     return (
       <div className="App">
-        <h1>Kamil FrontEnd!</h1>
+        <h1>Hello from the frontend!</h1>
         <h1>{this.state.response.body}</h1>
       </div>
     );
   }
 }
+
 
 export default App;
