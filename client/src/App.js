b9e343c89f700bb3bb7e144fc16ab5f8550971e9
Kamil Bronisz <kam.bronisz@gmail.com>
2020-08-30 10:20:05 +0200

    desctription updatea

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello from!</h1>
+        <h1>Kamil FrontEnd!</h1>
         <h1>{this.state.response.body}</h1>
       </div>
     );
